50b6c6ad6527dfbfa49f42eac405c750eb059e71
Matt McCorry <mccorry@gmail.com>
2026-02-09 19:40:09 +0000

    more tests

--- UnitTests/BHCCMendixConstants.cs
+++ UnitTests/BHCCMendixConstants.cs
@@ -5,4 +5,5 @@ public static class BHCCMendixConstants
     public const string CollectionsCollection = "32088147";
     public const string BHCCThemeAddress = "309622";
     public const string BHCCThemeAddressTempTable = "1491819";
+    public const string UprnChangeElement = "14918190";
 }
--- UnitTests/MendixClientTests.cs
+++ UnitTests/MendixClientTests.cs
@@ -86,7 +86,9 @@ public class MendixClientTests
         && ComparisonTools.HasKeys(dto.Changes, new[] { BHCCMendixConstants.BHCCThemeAddress, BHCCMendixConstants.CollectionsCollection, BHCCMendixConstants.BHCCThemeAddressTempTable })
         && ComparisonTools.GetKeyValue(dto.Changes, BHCCMendixConstants.BHCCThemeAddress)["SearchString"].Value == "BN1 8NT"
         && ComparisonTools.GetKeyValue(dto.Changes, BHCCMendixConstants.BHCCThemeAddress)["Collections.Collection_Address"].Value.StartsWith(BHCCMendixConstants.CollectionsCollection)
+        && ComparisonTools.GetKeyValue(dto.Changes, BHCCMendixConstants.BHCCThemeAddress)["BHCCTheme.AddressTemp_SelectedAddress"].Value.StartsWith(BHCCMendixConstants.UprnChangeElement)
         && ComparisonTools.GetKeyValue(dto.Changes, BHCCMendixConstants.CollectionsCollection)["DisplayCollectionsButton"].Value == "True"
+        ////&& ComparisonTools.GetKeyValue(dto.Changes, BHCCMendixConstants.UprnChangeElement)["BHCCTheme.AddressTemp_SelectedAddress"].Value.StartsWith(BHCCMendixConstants.BHCCThemeAddress)
         && dto.Changes.Count == 44
         && ComparisonTools.HasGuids(dto.Objects, new[] { BHCCMendixConstants.BHCCThemeAddress, BHCCMendixConstants.CollectionsCollection, BHCCMendixConstants.BHCCThemeAddressTempTable })
         && dto.Objects.Length == 44;
