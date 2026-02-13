be4fd1492493ebcabde6f00c749a1ce6ac61a793
Matt McCorry <mccorry@gmail.com>
2026-02-13 08:13:30 +0000

    Reference Python Mendix waste collection example
    Added a comment linking to a Python Mendix example (knowsley_gov_uk.py) from the hacs_waste_collection_schedule repository for potential inspiration or comparison regarding waste collection schedule integration.

--- BrightonBins/Program.cs
+++ BrightonBins/Program.cs
@@ -4,6 +4,7 @@ using BrightonBins.Client;
 // To run in browser, start here:
 // https://enviroservices.brighton-hove.gov.uk/link/collections
 // 3208814
+// Python Mendix example: https://github.com/mampfes/hacs_waste_collection_schedule/blob/master/custom_components/waste_collection_schedule/waste_collection_schedule/source/knowsley_gov_uk.py
 
 Console.WriteLine("Brighton Bins Collection Lookup");
 
