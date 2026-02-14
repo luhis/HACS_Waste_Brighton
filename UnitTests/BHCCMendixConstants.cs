4c86aa3d0c10081c1791e14ad96d48e3a5a750a3
Matt McCorry <mccorry@gmail.com>
2026-02-14 20:22:09 +0000

    extend constants values

--- UnitTests/BHCCMendixConstants.cs
+++ UnitTests/BHCCMendixConstants.cs
@@ -2,8 +2,8 @@
 
 public static class BHCCMendixConstants
 {
-    public const string CollectionsCollection = "32088147";
-    public const string BHCCThemeAddress = "309622";
+    public const string CollectionsCollection = "3208814765";
+    public const string BHCCThemeAddress =      "309622517";
     public const string BHCCThemeAddressTempTable = "1491819";
-    public const string UprnChangeElement = "14918190";
+    public const string UprnChangeElement = "14918190"; // is this correct?
 }
